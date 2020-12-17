863dd1b5b2c23bcd8fed39319e413a807a510f7b
bbn <clfbbn@gmail.com>
2020-12-17 12:09:30 +0800

    [build-script] Use updated defconfig
    Signed-off-by: bbn <clfbbn@gmail.com>

--- build.sh
+++ build.sh
@@ -8,17 +8,17 @@ CLANG_PATH=/run/media/bbn/7b433792-2537-4661-97dd-14052ca9b4b9/LineageOS/prebuil
 BUILD_CROSS_COMPILE=${GCC_PATH}/aarch64-linux-gnu-
 CLANG_TRIPLE=aarch64-linux-gnu-
 
-DEFCONFIG=vendor/kona-perf_defconfig
+DEFCONFIG=kona-bbn_defconfig
 
-export KBUILD_DIFFCONFIG=pdx203_j_diffconfig
+# export KBUILD_DIFFCONFIG=pdx203_j_diffconfig
 export PATH=${CLANG_PATH}:${PATH}
 
-make -j56 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
+make -j16 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
     CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
     CROSS_COMPILE=$BUILD_CROSS_COMPILE $DEFCONFIG
 
-make -j56 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
-    CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
-    CROSS_COMPILE=$BUILD_CROSS_COMPILE 2>&1 | tee build.txt
+ make -j16 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
+     CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
+     CROSS_COMPILE=$BUILD_CROSS_COMPILE 2>&1 | tee build.txt
  
 cp out/arch/arm64/boot/Image $(pwd)/boot.img-zImage
