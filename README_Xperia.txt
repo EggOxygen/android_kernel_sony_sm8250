1ec9aa61389dd7330688842fea88c251df59609c
Luofan Chen <clfbbn@gmail.com>
2021-01-20 15:42:42 +0800

    kernel: import source code from 58.0.A.4.463
    Signed-off-by: Luofan Chen <clfbbn@gmail.com>

---
+++ README_Xperia.txt
@@ -0,0 +1,7 @@
+Configuration files can be found in arch/arm64/configs.
+
+  defconfig using in common:
+    kona-perf_defconfig
+
+  diffconfigs for each product:
+    Xperia 1 II XQ-AT51/XQ-AT52/XQ-AT72/XQ-AT42 => pdx203_diffconfig
---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+mkdir -p out
+
+GCC_PATH=/run/media/bbn/7b433792-2537-4661-97dd-14052ca9b4b9/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin
+CLANG_PATH=/run/media/bbn/7b433792-2537-4661-97dd-14052ca9b4b9/LineageOS/prebuilts/clang/host/linux-x86/clang-r353983c1/bin
+
+BUILD_CROSS_COMPILE=${GCC_PATH}/aarch64-linux-gnu-
+CLANG_TRIPLE=aarch64-linux-gnu-
+
+DEFCONFIG=vendor/kona-perf_defconfig
+
+export KBUILD_DIFFCONFIG=pdx203_diffconfig
+export PATH=${CLANG_PATH}:${PATH}
+
+if [[ -z "$1" ]]
+    then
+    make -j16 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
+        CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE=$BUILD_CROSS_COMPILE ${DEFCONFIG}
+
+    make -j16 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
+        CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE=$BUILD_CROSS_COMPILE 2>&1 | tee build.txt
+ 
+    else
+       make -j16 -C $(pwd) O=$(pwd)/out ARCH=arm64 \
+        CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE=$BUILD_CROSS_COMPILE $1
+
+fi 
+# cp out/arch/arm64/boot/Image $(pwd)/boot.img-zImage
